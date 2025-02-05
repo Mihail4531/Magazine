3e185b399e35d9a7c2e56686026943f437c9021a
Mihail4531 <borovopaziloj4@gmail.com>
2025-02-05 20:12:36 +0300

    Добавил секцию welcome и стили

--- assets/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const hamb = document.querySelector("#hamb");
-const phone = document.querySelector("#phone");
-hamb.addEventListener("click", hambHandler);
-function  hambHandler (e){
-    e.preventDefault();
-    hamb.classList.toggle("active");
-    phone.classList.toggle("phone--active")
-}
